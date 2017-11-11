02dd8014d691909d17c791f70a620955c2128ae5
Filip Sobczak <filsob@gmail.com>
2017-11-11 17:08:28 +0000

    uppercase action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,8 +1,8 @@
 // Controls reducers
-export const SPEED_UP = 'speed_up';
-export const SPEED_DOWN = 'speed_down';
-export const TOGGLE_PAUSE = 'toggle_pause';
+export const SPEED_UP = 'SPEED_UP';
+export const SPEED_DOWN = 'SPEED_DOWN';
+export const TOGGLE_PAUSE = 'TOGGLE_PAUSE';
 
 // Board reducers
-export const NEXT_STEP = 'next_step';
-export const TOGGLE_CELL = 'toggle_cell';
+export const NEXT_STEP = 'NEXT_STEP';
+export const TOGGLE_CELL = 'TOGGLE_CELL';
